5cd1f4939bc538d3cf73231e903f01ab60133cba
anjrs <anjarasoaramanohery@gmail.com>
2025-03-23 19:41:59 +0300

    test front

---
+++ src/main/java/com/example/demo/config/JspConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class JspConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(@SuppressWarnings("null") ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void configureViewResolvers(@SuppressWarnings("null") ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        registry.viewResolver(resolver);
+    }
+}
